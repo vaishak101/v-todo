fd8b149258f769664cc5ca9062f30a52095a724f
vaishak101 <nairvaishak101@gmail.com>
2022-01-14 16:27:15 +0530

    CRUD operations Done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 const nav = document.querySelector('.nav');
 const taskBox = document.querySelectorAll('.task__box');
 nav.addEventListener('click', function (e) {
@@ -8,7 +9,6 @@ nav.addEventListener('click', function (e) {
     const test = e.target.getAttribute('data-num');
     taskBox.forEach(s => s.classList.remove('active'));
     taskBox[test].classList.add('active');
-    console.log(test);
   } else {
     return;
   }
@@ -17,10 +17,14 @@ nav.addEventListener('click', function (e) {
 const InputTitle = document.querySelector('.title');
 const InputDateTime = document.querySelector('.date');
 const addBtn = document.querySelector('.form__btn');
+const InputTitleUpdate = document.querySelector('.title--modal');
+const InputDateTimeUpdate = document.querySelector('.date--modal');
+const addBtnUpdate = document.querySelector('.form__btn--modal');
 const allTask = document.querySelector('.content--allTask');
 const updateTask = document.querySelector('.content--Update');
 const taskBoxUP = document.querySelectorAll('.content--up');
-
+const modal = document.querySelector('.modal');
+let getBtnIDEdit;
 class Task {
   constructor(id, title, dateTime) {
     this.id = id;
@@ -31,12 +35,13 @@ class Task {
 
 class App {
   #task = [];
+
   constructor() {
     this.getLocalstorage();
     addBtn.addEventListener('click', this._newTask.bind(this));
-    console.log(updateTask);
     updateTask.addEventListener('click', this.deleteTask.bind(this));
-    console.log('app constructor');
+    addBtnUpdate.addEventListener('click', this.editTask.bind(this));
+    console.log(this.#task);
   }
 
   renderTask(task) {
@@ -53,14 +58,15 @@ class App {
     </li>
     `;
     const htmlForUpdate = `<li class="task taskd" data-id="${task.id}">
-    <button class="task__delete" data-id="${task.id}">Delete</button>
     <div class="task__details">
     <span class="workout__value">${task.title}</span>  
     </div>
     <div class="task__details">
-          <span class="workout__icon">⏱</span>
-          <span class="workout__value">${task.dateTime}</span> 
+    <span class="workout__icon">⏱</span>
+    <span class="workout__value">${task.dateTime}</span> 
     </div>
+    <button class="task__update" data-id="${task.id}">Edit</button>
+    <button class="task__delete" data-id="${task.id}">Delete</button>
     </li>`;
     allTask.insertAdjacentHTML('beforeend', htmlforView);
     updateTask.insertAdjacentHTML('beforeend', htmlForUpdate);
@@ -92,6 +98,22 @@ class App {
     this.setLocalstorage();
     this.resetValue();
   }
+  editTask(e) {
+    const idUpdated = Date.now();
+    //Update new value
+    const title = InputTitleUpdate.value;
+    const dateTime = InputDateTimeUpdate.value;
+    const task = this.#task.find(o => o.id == getBtnIDEdit);
+    task.title = title;
+    task.dateTime = dateTime;
+    task.id = idUpdated;
+    //render new data
+    this.setLocalstorage();
+    taskBoxUP.forEach(s => (s.innerHTML = ''));
+    this.getLocalstorage();
+    this.resetValue();
+    modal.classList.add('modal--hide');
+  }
   deleteTask(e) {
     if (e.target.classList.contains('task__delete')) {
       const getBtnID = e.target.getAttribute('data-id');
@@ -100,13 +122,25 @@ class App {
       this.setLocalstorage();
       taskBoxUP.forEach(s => (s.innerHTML = ''));
       this.getLocalstorage();
+    } else if (e.target.classList.contains('task__update')) {
+      modal.classList.remove('modal--hide');
+      getBtnIDEdit = e.target.getAttribute('data-id');
+      let taskEdit;
+      this.#task.forEach(i =>
+        i.id == getBtnIDEdit ? (taskEdit = i) : console.log('no')
+      );
+      InputTitleUpdate.value = taskEdit.title;
+      InputDateTimeUpdate.value = taskEdit.dateTime;
     } else {
       return;
     }
   }
-  updateTask(e) {}
   resetValue() {
-    InputTitle.value = InputDateTime.value = '';
+    InputTitle.value =
+      InputDateTime.value =
+      InputTitleUpdate.value =
+      InputDateTimeUpdate.value =
+        '';
   }
   setLocalstorage() {
     localStorage.setItem('tasks', JSON.stringify(this.#task));
@@ -119,3 +153,9 @@ class App {
   }
 }
 const app = new App();
+
+//MODAL CLOSING
+const closeModal = document.querySelector('.modal__btn--close');
+closeModal.addEventListener('click', function () {
+  modal.classList.add('modal--hide');
+});
